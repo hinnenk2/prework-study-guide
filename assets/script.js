7fe5e24577d17e02aee569ebd64f2dd9f7502f6b
hinnenk2 <124109780+hinnenk2@users.noreply.github.com>
2023-02-12 13:16:58 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
 function listTopics() {
     for (var x = 0; x < topics.length; x++) {
       console.log(topics[x]);
